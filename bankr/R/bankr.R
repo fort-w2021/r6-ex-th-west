25ebb0b438cae4eb3b336de991f0534735d87cab
th-west <twestermeier@live.de>
2021-02-02 11:41:59 +0100

    added giro account with extended withdraw method, overdraft limit and fee

--- bankr/R/bankr.R
+++ bankr/R/bankr.R
@@ -19,7 +19,7 @@ account <- R6::R6Class("Account",
     #' @param value the amount of money to withdraw from the account
     withdraw = function(value) {
       checkmate::assert_number(value, lower = 0)
-      if (value > self$balance) {
+      if (value > self$balance && class(self)[[1]] == "Account") {
         stop("You cannot withdraw more than ", self$balance, ".")
       }
       self$balance <- self$balance - value
@@ -28,17 +28,32 @@ account <- R6::R6Class("Account",
   )
 )
 
+#' R6 Subclass for a giro account
+#'
+#' @description
+#' A special form of bank account that has an overdraft limit and fee
 giro_account <- R6::R6Class("GiroAccount",
   inherit = account,
   public = list(
+    #' @description
+    #' method to withdraw money from giro account
+    #' @param value the amount of money to withdraw from account
     withdraw = function(value) {
+      if (self$balance - value - private$overdraft_fee < private$overdraft_limit) {
+        stop("Balance needs to be at least ", private$overdraft_limit, ".")
+      }
+      if (self$balance - value < 0) {
+        super$withdraw(value + private$overdraft_fee)
+      } else{
+        super$withdraw(value)
+      }
 
     }
-  )
+  ),
   private = list(
-    #' @field overdraft limit
-    overdraft_limit = -50
-    #' @field overdraft fee
-    overdraft_fee = - 5
+    # @field overdraft_limit the overdraft limit of given account
+    overdraft_limit = -500,
+    # @field overdraft_fee overdraft fee that gets deducted if balance is negative
+    overdraft_fee = 5
   )
 )
